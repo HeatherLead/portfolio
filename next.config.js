8338d7ece1e4f3a5a8153b7309d5e670799a047b
Ayush Bangar <143717997+HeatherLead@users.noreply.github.com>
2024-02-02 12:42:27 +0530

    deploy

--- next.config.js
+++ next.config.js
@@ -2,11 +2,3 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
-module.exports = {
-  experimental: {
-    optimizeCss: true,
-    optimizeImages: true,
-    optimizeFonts: true,
-    automaticStaticOptimization: false,
-  },
-};
